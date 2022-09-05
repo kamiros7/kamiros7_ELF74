f991b899208d7aa685714cb21b2461c68551e74e
Camilo <kamiro.cm37@gmail.com>
2022-09-05 17:27:46 -0300

    Modificações no Lab2 e inserção parcial do Lab3
    Lab2: Retirada do arquivo.dep, como também modificado o path dos arquivos buttons.c e pinout.c, sendo agora advindos da pasta drivers contida nativamente dentro da pasta examples/boards../drivers
    Lab3: Implementação parcial, faltando apenas a questão de printar corretamente os valores contidos no vetor do histograma.

---
+++ Lab3/main.cpp
@@ -0,0 +1,105 @@
+/*__________________________________________________________________________________
+|       Disciplina de Sistemas Embarcados - 2022-1
+|       Prof. Douglas Renaux
+| __________________________________________________________________________________
+|
+|		Lab 1
+| __________________________________________________________________________________
+*/
+
+/**
+ * @file     main.cpp
+ * @author   Luis Camilo Jussiani Moreira e João Victor Laskoski
+ * @brief    Solution to Lab1 of ELF74/CSW41 - UTFPR. \n 
+ *           Tools instalation and validation procedure.\n 
+ *           Show messages on terminal using std::cout. \n 
+ *           Show current value of some predefined macros (preprocessor symbols).\n 
+ *           Read float value from terminal using std::cin.
+ * @version  V2 -for 2022-2 semester
+ * @date     Sep, 2022
+ ******************************************************************************/
+
+/*------------------------------------------------------------------------------
+ *
+ *      Use Doxygen to report lab results
+ *
+ *------------------------------------------------------------------------------*/
+/** @mainpage Results from Lab1
+ *
+ * @section Ouput Values
+ *
+ * The values of ...
+ *
+ * @section Terminal
+ *
+ * @subsection Output
+ *
+ * etc...
+ */
+
+/*------------------------------------------------------------------------------
+ *
+ *      File includes
+ *
+ *------------------------------------------------------------------------------*/
+#include <stdint.h>
+
+#include <iostream>
+using std::cout;
+
+#include "template.h"
+#include "images.c"
+
+extern "C" uint16_t EightBitHistogram(uint16_t width, uint16_t height, const uint8_t * p_image, uint16_t * p_histogram);
+/*------------------------------------------------------------------------------
+ *
+ *      Typedefs and constants
+ *
+ *------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+ *
+ *      Global vars
+ *
+ *------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+ *
+ *      File scope vars
+ *
+ *------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+ *
+ *      Functions and Methods
+ *
+ *------------------------------------------------------------------------------*/
+
+void print_histogram(uint16_t *histogram){
+  for(uint16_t i = 0; i < 256; i++){
+    uint16_t a = histogram[i];
+    std::cout << "[ " << i << " ]: " << a << std::endl;
+  }
+}
+
+/**
+ * Main function.
+ *
+ * @param[in] argc - not used, declared for compatibility
+ * @param[in] argv - not used, declared for compatibility
+ * @returns int    - not used, declared for compatibility
+ */
+
+
+int main(int argc, char ** argv)
+{
+    uint16_t histogram[256];
+    
+    std::cout << "endereco = " << histogram << std::endl;
+    
+    EightBitHistogram(width0, height0, *image0, histogram);
+    
+    print_histogram(histogram);
+    
+    return 0;
+}
